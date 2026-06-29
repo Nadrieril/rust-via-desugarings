41772eb74cf36f39efaac997a2f37dab938bb436
Nadrieril <nadrieril+git@gmail.com>
2026-06-29 23:34:05 +0200

    Add some Reference links

--- src/book/pipeline/funsig.md.rs
+++ src/book/pipeline/funsig.md.rs
@@ -25,6 +25,7 @@ fn implicit_return(f: &mut Function) {
 }
 
 //@ A `self` parameter is only allowed as the first function argument
+//@ [[items.fn.syntax](https://doc.rust-lang.org/reference/items/functions.html#r-items.fn.syntax)].
 fn self_first(f: &Function) -> Result<(), CompilationError> {
     for p in f.parameters.iter().skip(1) {
         if !matches!(p.kind, FunctionParamKind::Regular { .. }) {
@@ -36,6 +37,7 @@ fn self_first(f: &Function) -> Result<(), CompilationError> {
 
 //@ A `self` parameter is sugar for `self: Self`, and a `&self` parameter is sugar for `self:
 //@ &Self`.
+//@ [[associated.fn.method.self-pat-shorthands](https://doc.rust-lang.org/reference/items/associated-items.html#r-associated.fn.method.self-pat-shorthands)].
 fn shorthand_self(f: &mut Function) {
     if let Some(p) = f.parameters.first_mut() {
         match &p.kind {
